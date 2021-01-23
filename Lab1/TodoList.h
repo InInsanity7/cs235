ad7e3224c563d98e345635868f3051275346351a
Nathan Poston <67882689+InInsanity7@users.noreply.github.com>
2021-01-23 14:16:52 -0700

    saving

--- Lab1/TodoList.h
+++ Lab1/TodoList.h
@@ -12,23 +12,33 @@ class TodoList: public TodoListInterface {
        ~TodoList() {
            std::cout << "In Destructor" << std::endl;
        }
-       void Add(std::string &userDate, std::string &userTask) {
+       void Add(std::string userDate, std::string userTask) {
            std::cout << "In Add" << std::endl;
            date.push_back(userDate);
            task.push_back(userTask);
        }
-       int Remove(std::string task) {
+       int Remove(std::string userTask) {
            std::cout << "In Remove" << std::endl;
            return 0;
        }
        void PrintTodoList() {
-           /* for (int i = 0; i < task.size(); i++) {
+           for (int i = 0; i < task.size(); i++) {
            std::cout << date.at(i) << std::endl;
            std::cout << task.at(i) << std::endl;
-           } */
+           } 
        }
-       void PrintDaysTasks(std::string date) {
+       void PrintDaysTasks(std::string userDate) {
            std::cout << "In PrintTodoList" << std::endl;
+           bool taskFound = false;
+
+                  for (int i = 0; (i < task.size()) && (!taskFound); i++) {
+                      if (userDate.compare(task.at(i))==0) {
+                          std::cout << date.at(i) << std::endl;
+                          std::cout << task.at(i) << std::endl;
+
+                          taskFound = true;
+                      }
+                  }
        }
 
     private:
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,23 +1,59 @@
 #include "TodoList.h"
 
-int main(int argc, char *argv[])
-{
-    TodoList testList;
+int main(int argc, char *argv[]) {
+    std::string argOne;
+    std::string argTwo;
+    std::string argThree;
 
-    for (int i = 0; i < argc; i++)
-    {
-        std::cout << "argv[" << i << "]=" << argv[i] << std::endl;
+    //validate usage:
+    if (argc < 2 || argc > 4) {
+        std::cout << "Usage: ./TODO <function> [option] [option1]" << std::endl;
     }
+    else {
+        
+        TodoList testList;
 
-    std::string argOne = argv[1];
-    std::string argTwo = argv[2];
+        /*
+        for (int i = 0; i < argc; i++) {
+        std::cout << "argv[" << i << "]=" << argv[i] << std::endl;
+        }
 
-    if (argOne.compare("add")==0)
-    {
-        std::cout << "Adding" << std::endl;
+        std::string argOne = argv[1];
+        std::string argTwo = argv[2];
+        */
 
-        testList.Add(argOne, argTwo);
-        // testList.Print();
+        if (argOne.compare("add")==0) {
+            std::cout << "Adding" << std::endl;
+
+            testList.Add(argv[1], argv[2]);
+            testList.PrintTodoList(); //FIXME test
+        }
+        else if (argOne.compare("remove")==0) {
+            if (argc != 3) {
+                std::cout << "Usage: ./TODO remove [task]" << std::endl;
+            }
+            else {
+                argTwo = argv[2];
+                testList.remove(argTwo);
+            }
+        }
+        else if (argOne.compare("printList")==0) {
+            if (argc != 2) {
+                std::cout << "Usage: ./TODO printList" << std::endl;
+            }
+            else {
+                testList.PrintTodoList();
+            }
+        }
+        else if (argOne.compare("printDay"==0) {
+            if (argc != 3) {
+                std::cout << "Usage ./TODO printDay [day]" << std::endl;
+            }
+            else {
+                //FIXME add check [day]
+
+                testList.PrintDaysTasks(argv[2]);
+        }
     }
 
     return 0;
